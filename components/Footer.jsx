9b0e498a4cba19f3923b3dd70812d7632b3f68fd
Jakub Sekula <seklerek@gmail.com>
2022-08-05 23:56:49 +0200

    Coming soon page

--- components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = ({ title, ...props }) => {
-  return (
-    <footer className="w-screen h-24 bg-slate-200 text-center shrink-0 flex justify-center items-center">
-      <h5>Copyright tonecarrier 2022</h5>
-    </footer>
-  );
-};
-
-export default Footer;
--- pages/auth.jsx
+++
@@ -1,8 +0,0 @@
-import { useAuth } from "../components/contexts/AuthContext";
-
-const App = (props) => {
-  const { auth } = useAuth();
-  return <pre>{JSON.stringify(auth, null, "\t")}</pre>;
-};
-
-export default App;
