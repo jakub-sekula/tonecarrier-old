5aaaae7a3af0c30a5b5335ddee1782c3073e7eba
Jakub Sekula <seklerek@gmail.com>
2024-05-19 01:58:46 +0100

    Add negative view to sample gallery

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -1,8 +1,14 @@
 "use client";
 import clsx from "clsx";
-import { Camera, Download, Film, SunMedium } from "lucide-react";
+import {
+  Camera,
+  Download,
+  Film,
+  MousePointerClick,
+  SunMedium,
+} from "lucide-react";
 import Image from "next/image";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Autoplay from "embla-carousel-autoplay";
 
 import {
@@ -16,6 +22,7 @@ import {
 type Photo = {
   name: string;
   image: string;
+  original?: string;
   camera?: string;
   film?: string;
   light?: string;
@@ -25,6 +32,7 @@ const initialPhotos: Photo[] = [
   {
     name: "Photo 1",
     image: "/images/samples/1.jpg",
+    original: "/images/samples/1a.jpg",
     camera: "Canon 70D",
     // film: "Kodak Gold 200",
     light: "iPad Air 2",
@@ -32,6 +40,7 @@ const initialPhotos: Photo[] = [
   {
     name: "Photo 2",
     image: "/images/samples/2.jpg",
+    original: "/images/samples/2a.jpg",
     camera: "Canon 70D",
     // film: "Kodak Gold 200",
     light: "iPad Air 2",
@@ -39,6 +48,7 @@ const initialPhotos: Photo[] = [
   {
     name: "Photo 3",
     image: "/images/samples/3.jpg",
+    original: "/images/samples/3a.jpg",
     camera: "Canon 70D",
     // film: "Kodak Gold 200",
     light: "iPad Air 2",
@@ -46,6 +56,7 @@ const initialPhotos: Photo[] = [
   {
     name: "Photo 4",
     image: "/images/samples/4.jpg",
+    original: "/images/samples/4a.jpg",
     camera: "Canon 70D",
     // film: "Kodak Gold 200",
     light: "iPad Air 2",
@@ -53,6 +64,7 @@ const initialPhotos: Photo[] = [
   {
     name: "Photo 5",
     image: "/images/samples/5.jpg",
+    original: "/images/samples/5a.jpg",
     camera: "Canon 70D",
     // film: "Kodak Gold 200",
     light: "iPad Air 2",
@@ -60,6 +72,7 @@ const initialPhotos: Photo[] = [
   {
     name: "Photo 5",
     image: "/images/samples/6.jpg",
+    original: "/images/samples/6a.jpg",
     camera: "Canon 70D",
     // film: "Kodak Gold 200",
     light: "iPad Air 2",
@@ -67,6 +80,7 @@ const initialPhotos: Photo[] = [
   {
     name: "Photo 5",
     image: "/images/samples/7.jpg",
+    original: "/images/samples/7a.jpg",
     camera: "Canon 70D",
     // film: "Kodak Gold 200",
     light: "iPad Air 2",
@@ -74,6 +88,7 @@ const initialPhotos: Photo[] = [
   {
     name: "Photo 5",
     image: "/images/samples/8.jpg",
+    original: "/images/samples/8a.jpg",
     camera: "Canon 70D",
     // film: "Kodak Gold 200",
     light: "iPad Air 2",
@@ -81,40 +96,38 @@ const initialPhotos: Photo[] = [
 ];
 
 function PhotoComponent({ photo }: { photo: Photo }) {
+  const [invert, setInvert] = useState(false);
   return (
     <div
+      onClick={() => setInvert((prev) => !prev)}
       className={clsx(
-        "md:hover:scale-[102.5%] md:hover:shadow-xl transition-all duration-200",
+        "group md:hover:shadow-xl transition-all duration-200",
         " w-full shrink-0 md:h-full md:rounded overflow-hidden group relative  md:aspect-[3/2] border md:border-none"
       )}
     >
-      <div className="md:group-hover:opacity-100 bg-black/50 backdrop-blur-sm transition-all duration-200 absolute inset-0 opacity-0 text-white p-6 flex flex-col gap-4">
+      <div className="md:group-hover:opacity-100 bg-black/20 border border-white/20 shadow-xl backdrop-blur-md z-50 ransition-all duration-200 absolute  top-4 left-4 opacity-0 text-white px-3 py-2 rounded flex flex-col gap-0 text-sm">
         <div className="flex items-center gap-2">
-          <Camera className="size-5 text-white/75" />
+          <Camera className="size-4" />
           <span>{photo.camera}</span>
         </div>
         {photo.film ? (
           <div className="flex items-center gap-2">
-            <Film className="size-5 text-white/75" />
+            <Film className="size-4" />
             <span>{photo.film}</span>
           </div>
         ) : null}
 
         <div className="flex items-center gap-2">
-          <SunMedium className="size-5 text-white/75" />
+          <SunMedium className="size-4" />
           <span>{photo.light}</span>
         </div>
-        {/* <button className="w-full mt-auto py-2 rounded border-white border bg-white/10 hover:bg-white/70 hover:border-transparent text-white flex justify-center items-center gap-2">
-          <Download className="size-4" />
-          Download RAW
-        </button> */}
       </div>
       <Image
         width={400}
         height={267}
-        src={photo.image}
+        src={!invert ? photo.image : photo?.original || photo.image}
         alt={photo.name}
-        className="w-full md:h-full object-cover"
+        className="w-full md:h-full object-cover md:group-hover:scale-[102.5%] transition-all duration-300"
       />
       <div className="p-6 flex flex-col gap-2 md:hidden">
         <div className="flex items-center gap-2">
@@ -148,9 +161,15 @@ export default function Gallery() {
         {/* Section header */}
         <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16 flex flex-col gap-4">
           <h2 className="h2">Sample scans</h2>
-          <p className="text-lg md:text-xl text-gray-600" >
+          <p className="text-lg md:text-xl text-gray-600">
             Download a few of our example files in RAW (DNG) format.
           </p>
+          <div className="flex text-gray-600 mx-auto gap-1 items-center ">
+            <span className="inline-flex gap-1 font-bold items-center">
+            <MousePointerClick className="size-6" />Click
+            </span>{" "}
+            on any image to view the original negative.
+          </div>
           <div
             className="mx-auto sm:max-w-none sm:flex sm:justify-center"
             data-aos="zoom-y-out"
